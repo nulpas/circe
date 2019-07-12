49f3302e047a71d8fb58d99f868001484b6099c1
nulpas <manu@lunaeme.com>
2019-07-12 09:53:17 +0200

    Nul Pàs :: Circe v.0.0.0-beta.30  · Upgraded all components.

--- projects/dropdown/src/lib/dropdown.module.ts
+++ projects/dropdown/src/lib/dropdown.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { DropdownComponent } from './dropdown.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { EventsService, ToolService } from '@lunaeme/circe-core';
+import { BoxModelService, EventsService, ToolService } from '@lunaeme/circe-core';
 import { DropdownEventsDirective } from './dropdown-events.directive';
 import { ClickOutsideModule } from '@lunaeme/circe-click-outside';
 
@@ -20,7 +20,8 @@ import { ClickOutsideModule } from '@lunaeme/circe-click-outside';
   ],
   providers: [
     ToolService,
-    EventsService
+    EventsService,
+    BoxModelService
   ]
 })
 export class DropdownModule {}
