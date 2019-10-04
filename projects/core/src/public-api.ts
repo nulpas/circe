f83994d41aa85ccf4d73e0a215d9a6c565ff5cf1
nulpas <manu@lunaeme.com>
2019-10-04 11:09:56 +0200

    Nul Pàs :: Circe v.0.0.0-beta.30  · Several fixes and updates to Core module.

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -3,6 +3,7 @@
  */
 
 export * from './lib/box-model.service';
+export * from './lib/custom-validation.service';
 export * from './lib/events.service';
 export * from './lib/tool.service';
 export * from './lib/_types/data.types';
