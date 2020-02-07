4ea2e3e82d26a9ab1008a8d0cc83870d7144c9d3
nulpas <manu@lunaeme.com>
2020-02-07 09:43:22 +0100

    Nul Pàs :: Circe v.0.0.0-beta.40  · Configured CI/CD.  · First deploy attempt.

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+ssh -o "StrictHostKeyChecking no" travis@188.166.18.204 -p 18665 /bin/bash << EOF
+  cd /var/www/circe/web
+  docker login repo.treescale.com --username=nulpas --password=$TREESCALE_PS
+  docker rm playbook --force
+  docker pull repo.treescale.com/nulpas/sds-circe:latest
+  docker run --name circe -d -p 6161:80 repo.treescale.com/nulpas/sds-circe:latest
+EOF
