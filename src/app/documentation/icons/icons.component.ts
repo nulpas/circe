870d2304e3d0e6ec330b013446dd3de3ff439b4d
nulpas <manu@lunaeme.com>
2020-02-16 08:54:48 +0100

    Nul Pàs :: Circe v.0.0.14  . Activated icons filter.

--- src/app/documentation/icons/icons.component.ts
+++ src/app/documentation/icons/icons.component.ts
@@ -5,6 +5,7 @@ import { concat, Subject } from 'rxjs';
 import { Icon, IconSection, IconSectionsRequest } from '../../_types/response.types';
 import { ToolService } from '@core/tool.service';
 import { OrderPipe } from '@core/external.elements';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-icons',
@@ -17,6 +18,7 @@ export class IconsComponent implements OnInit, OnDestroy {
   public totalIcons: number = 0;
   public sections: Array<IconSection> = [];
   public icons: {[section: string]: Array<Icon>} = {};
+  public iconsFilter: FormControl = new FormControl('');
 
   private _sourceSections: Array<IconSection>;
   private _sourceIcons: Array<Icon>;
@@ -35,21 +37,18 @@ export class IconsComponent implements OnInit, OnDestroy {
       takeUntil(this._componentDestroyed$)
     ).subscribe((r: IconSectionsRequest | Array<Icon>) => {
       if ((r as IconSectionsRequest).isIconSections) {
-        this._sourceSections = (r as IconSectionsRequest).iconSections;
-        this._sourceSections = _order.transform(this._sourceSections, 'name');
+        this._sourceSections = _order.transform((r as IconSectionsRequest).iconSections, 'name');
       } else {
         this._sourceIcons = _order.transform(r, 'code');
-        this.totalIcons = this._sourceIcons.length;
-        for (const section of this._sourceSections) {
-          const _sectionIcons: Array<Icon> = this._sourceIcons.filter((e: Icon) => e.sections && e.sections.includes(section.id));
-          if (_sectionIcons.length) {
-            this.icons[section.id] = _sectionIcons;
-          }
-        }
-        const _visibleSections = Object.keys(this.icons);
-        this.sections = this._sourceSections.filter((e: IconSection) => _visibleSections.includes(e.id));
+        this.iconsDataForRender(this._sourceIcons);
       }
-      _cd.markForCheck();
+    });
+
+    this.iconsFilter.valueChanges.pipe(
+      takeUntil(this._componentDestroyed$)
+    ).subscribe((r: string) => {
+      const _filteredSourceIcons: Array<Icon> = this._sourceIcons.filter((e: Icon) => e.code.includes(r));
+      this.iconsDataForRender(_filteredSourceIcons);
     });
   }
 
@@ -58,7 +57,25 @@ export class IconsComponent implements OnInit, OnDestroy {
     this.showStickyBox = (_scrollElement.scrollTop > 0);
   }
 
+  private iconsDataForRender(sourceIcons: Array<Icon>): void {
+    this.icons = {};
+    this.totalIcons = sourceIcons.length;
+    for (const section of this._sourceSections) {
+      const _sectionIcons: Array<Icon> = sourceIcons.filter((e: Icon) => e.sections && e.sections.includes(section.id));
+      if (_sectionIcons.length) {
+        this.icons[section.id] = _sectionIcons;
+      }
+    }
+    const _visibleSections = Object.keys(this.icons);
+    this.sections = this._sourceSections.filter((e: IconSection) => _visibleSections.includes(e.id));
+    this._cd.markForCheck();
+  }
+
   ngOnInit(): void {}
 
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+    this._componentDestroyed$.next();
+    this._componentDestroyed$.complete();
+    this._componentDestroyed$.unsubscribe();
+  }
 }
